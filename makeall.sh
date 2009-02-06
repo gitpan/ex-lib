406b90a2b76f99157458aa4390b3fa3d8da4bce5
Mons Anderson <mons@cpan.org>
2009-02-06 17:40:04 +0000

    Import of MONS/ex-lib-0.04 from CPAN.
    gitpan-cpan-distribution: ex-lib
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MONS/ex-lib-0.04.tar.gz
    gitpan-cpan-author:       MONS
    gitpan-cpan-maturity:     released

--- makeall.sh
+++ makeall.sh
@@ -1,11 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-rm -rf MANIFEST.bak MANIFEST Makefile.old && \
-perl Makefile.PL && \
+MODULE=lib/ex/lib.pm
+perl=perl
+$perl -v
+
+rm -rf MANIFEST.bak MANIFEST Makefile.old META.yml && \
+echo > '_.tar.gz' && \
+pod2text $MODULE > README && \
+$perl -i -lpne 's{^\s+$}{};s{^    ((?: {8})+)}{" "x(4+length($1)/2)}se;' README && \
+$perl Makefile.PL && \
+rm *.tar.gz && \
 make manifest && \
-perl Makefile.PL && \
-make test && \
-make dist && \
-make disttest && \
+$perl -i -lne 'print unless /(?:\.tar\.gz$|^dist)/' MANIFEST && \
 make clean && \
+$perl Makefile.PL && \
+make && \
+make test && \
+make disttest && \
+make dist && \
+cp -f *.tar.gz dist/ && \
+make clean && \
+rm -rf MANIFEST.bak Makefile.old && \
 echo "All is OK"
